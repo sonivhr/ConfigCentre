4d132911e85b4fe64820c00c9bb1e6a4fbc29621
VishalSoni <sonivhr@gmail.com>
2023-01-25 02:20:41 +0530

    PUSH MESSAGE: ReceiverA shows configuration

--- receiverA/src/main/java/com/configcentre/projecta/MainActivity.kt
+++ receiverA/src/main/java/com/configcentre/projecta/MainActivity.kt
@@ -2,10 +2,34 @@ package com.configcentre.projecta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.configcentre.projecta.databinding.ActivityMainBinding
+import com.configcentre.projecta.model.Configuration
+import com.configcentre.projecta.util.EXTRA_CONFIGURATION
+import com.configcentre.projecta.util.SharedPreferenceManager
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val sharedPreferenceManager by lazy {
+        SharedPreferenceManager(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showConfig()
+    }
+
+    private fun showConfig() {
+        val configurationString = sharedPreferenceManager.getString(EXTRA_CONFIGURATION)
+        if (!configurationString.isNullOrEmpty()) {
+            Gson().fromJson(configurationString, Configuration::class.java).run {
+                binding.tvConfig.text =
+                    "Username: $userName\nPassword: $password\nMessage: $message"
+            }
+        }
     }
 }
--- receiverA/src/main/java/com/configcentre/projecta/util/SharedPreferenceManager.kt
+++ receiverA/src/main/java/com/configcentre/projecta/util/SharedPreferenceManager.kt
@@ -2,14 +2,17 @@ package com.configcentre.projecta.util
 
 import android.content.Context
 
-private const val SHARED_PREFERENCE_NAME = "MY_SHARED_PREFERENCES"
+private const val SHARED_PREFERENCE_NAME = "_SHARED_PREFERENCES"
 
 const val EXTRA_CONFIGURATION = "extra_configuration"
 
 class SharedPreferenceManager(context: Context) {
 
     private var sharedPreferences =
-        context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        context.getSharedPreferences(
+            /* name = */ "${context.packageName}$SHARED_PREFERENCE_NAME",
+            /* mode = */ Context.MODE_PRIVATE
+        )
 
     fun putString(prefKey: String, prefValue: String) {
         with(sharedPreferences.edit()) {
